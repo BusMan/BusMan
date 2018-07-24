34ba0f6ea5c74cd83e942880d95a20376f122530
David Zhao <davidzhao058@gmail.com>
2018-07-24 03:24:40 -0400

    Create static action bar

--- busman/static/js/busmap/busmap.vue
+++ busman/static/js/busmap/busmap.vue
@@ -2,28 +2,48 @@
   <div>
     <top-bar :message="message"></top-bar>
     <map-svg :selected="selected" :highlighted="highlighted"/>
+    <action-bar :actions="actions"></action-bar>
   </div>
 </template>
 
 <script>
 import topBar from './components/topBar.vue';
 import mapSvg from './components/mapSvg.vue';
+import actionBar from './components/actionBar.vue';
 
 export default {
   props: [
     'user',
-    'routes'
+    'routes',
   ],
   data: function () {
     return {
       'message': `Hi ${this.user.name}, you are cool.`,
       'selected': null,
-      'highlighted': null
+      'highlighted': null,
+      'actions': [
+        {
+          'text': 'Assign Bus',
+          'id': 'assign-bus',
+          'icon': 'fa-plus',
+        },
+        {
+          'text': 'Search',
+          'id': 'search',
+          'icon': 'fa-search',
+        },
+        {
+          'text': 'Mark Delayed',
+          'id': 'mark-delayed',
+          'icon': 'fa-clock',
+        },
+      ],
     }
   },
   components: {
     topBar,
     mapSvg,
+    actionBar,
   }
 }
 </script>
--- busman/static/js/busmap/components/actionBar.vue
+++ busman/static/js/busmap/components/actionBar.vue
@@ -1,12 +1,16 @@
 <template>
 <div class="actionbar">
+  <div v-for="action in actions" class="action">
+    <i :class="['fa', action.icon]"></i>
+    <span>{{ action.text }}</span>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
   props: [
-    'message'
+    'actions'
   ],
 }
 </script>
