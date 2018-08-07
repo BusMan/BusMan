d329f787f4620f6d367ba556ad712ac94a0e0c4b
David Zhao <davidzhao058@gmail.com>
2018-08-06 22:16:37 -0400

    Create static search overlay

---
+++ busman/static/js/busmap/components/searchOverlay.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="search-overlay">
+    <div class="search-bar">
+        <input type="text" placeholder="Search here...">
+        <span class="cancel-button">Cancel</span>
+    </div>
+    <div class="search-results">
+        <ul>
+            <li v-for="route in routes">{{ route.routeName }}</li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'routes',
+  ],
+  watch: {},
+  methods: {},
+}
+</script>
